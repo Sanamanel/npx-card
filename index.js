fe482d81ce9795d052f6a7f3fb2385a5da48b82e
User <rachida.haddouch.webdev@gmail.com>
2021-01-26 11:08:14 +0100

    style card update

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const data = {
   handle: chalk.white("Sanamanel"),
   work: chalk.white("Webdev at BeCode"),
   npm: chalk.blue.bold("https://npmjs.com/") + chalk.cyan("~sanamanel"),
-  github: chalk.blue.bold("https://github.com/") + chalk.magenta("Sanamanel"),
+  github: chalk.blue.bold("https://github.com/") + chalk.cyan("Sanamanel"),
   linkedin:
-    chalk.blue.bold("https://linkedin.com/in/") + chalk.green("rachida-webdev"),
-  web: chalk.magenta.bold("http://bout-de-creations.com/"),
+    chalk.blue.bold("https://linkedin.com/in/") + chalk.cyan("rachida-webdev"),
+  web: chalk.cyan.bold("http://bout-de-creations.com/"),
   npx: chalk.magenta("npx-card") + " " + chalk.white("Sanamanel"),
-  labelWork: chalk.bgWhite.blue.bold("Work:"),
-  labelNpm: chalk.bgWhite.blue.bold("Npm:"),
-  labelGitHub: chalk.bgWhite.blue.bold("GitHub:"),
-  labelLinkedIn: chalk.bgWhite.blue.bold("LinkedIn:"),
-  labelWeb: chalk.bgWhite.blue.bold("My Website:"),
-  labelCard: chalk.bgWhite.blue.bold("Card:"),
+  labelWork: chalk.bgWhite.magenta.bold("Work:"),
+  labelNpm: chalk.bgWhite.magenta.bold("Npm:"),
+  labelGitHub: chalk.bgWhite.magenta.bold("GitHub:"),
+  labelLinkedIn: chalk.bgWhite.magenta.bold("LinkedIn:"),
+  labelWeb: chalk.bgWhite.magenta.bold("My Website:"),
+  labelCard: chalk.bgWhite.magenta.bold("Card:"),
 };
 const line = "\n";
 const empty = "";
